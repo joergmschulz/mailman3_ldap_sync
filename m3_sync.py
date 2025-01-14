fa2a25b81013ebb8c93143c17cb56105d28cef92
Jörg Michael Schulz <info@joergschulz.de>
2025-01-14 22:16:59 +0100

    Update m3_sync.py

--- m3_sync.py
+++ m3_sync.py
@@ -193,7 +193,7 @@ class M3Sync(object):
             if os.environ.get('DEBUG_DEVELOP') == 'true':
                     pdb.set_trace()
             ldap_data[self.get_list(list_name)]['description'] =  str(group["description"])
-            ldap_data[self.get_list(list_name)]['mail'] =  str(group["mail"])
+            ldap_data[self.get_list(list_name)]['mail'] =  str(group["mail"][0])
 
             for attr in self.__attrs:
                 for dn in getattr(group, self.sync['{0}_attr'.format(attr)]):

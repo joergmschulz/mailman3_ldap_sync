5808c58a4575051bf7ebe7f96513bd25fa9a2c5c
Jörg Michael Schulz <info@joergschulz.de>
2025-08-22 12:59:47 +0200

    one more typo

--- m3_sync.py
+++ m3_sync.py
@@ -345,7 +345,7 @@ class M3Sync(object):
                     if os.environ.get('DEBUG_DEVELOP') == 'true':
                         pdb.set_trace()
                     mlist.remove_moderator(moderator.address.email)
-            if self.sync['delete_owners]:
+            if self.sync['delete_owners']:
               for owner in mlist.owners:
                 if owner.address.email not in ldap_data[list_name]['owner']:
                     self.logger.info(
